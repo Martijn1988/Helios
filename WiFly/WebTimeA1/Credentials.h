ddb34a87e93c80d8373b174f3851f9b42995bd1d
Martijn1988 <martijnbijwaard@gmail.com>
2014-02-02 15:58:01 +0100

    Start

---
+++ WiFly/WebTimeA1/Credentials.h
@@ -0,0 +1,9 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+// Wifi parameters
+char passphrase[] = "H3AGNAYNA43K";
+char ssid[] = "NSA_1566";
+char ntp_server[] = "nist1-la.ustiming.org"; // or your favorite ntp server.
+
+#endif
